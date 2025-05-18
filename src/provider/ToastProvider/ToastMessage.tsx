10b65999c79ea40424b1a4b781ec8ad44461a579
Thong Yuan Yu Sarah <thong.yuan.yu.sarah@gmail.com>
2025-05-18 22:11:07 +0800

    Fix build error

--- src/provider/ToastProvider/ToastMessage.tsx
+++ src/provider/ToastProvider/ToastMessage.tsx
@@ -1,5 +1,4 @@
 import clsx from "clsx";
-import React from "react";
 import { ToastContentProps } from "react-toastify";
 import { CloseIcon, ErrorIcon, InfoIcon, SuccessIcon, WarningIcon } from "../../assets";
 import { IconButton } from "../../components";
